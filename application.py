6421b6ff784af5497d953b995f8c44ef209cbed9
Doramy504 <121173601+ihorstnkv@users.noreply.github.com>
2025-03-30 13:24:43 +0300

    Add vulnerabilities for sast tool to detect

--- application.py
+++ application.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 
 application = Flask(__name__)
@@ -8,16 +7,19 @@ application = Flask(__name__)
 def root():
     return render_template("index.html")
 
+
 @application.route("/help")
 def helppage():
     return render_template("help.html")
+
 
 @application.route("/hello")
 def index():
     return "Hello World from Flask Hello Page.<b> v1.0"
 
-#--------Main------------------
+
+# --------Main------------------
 if __name__ == "__main__":
-    application.debug = True
-    application.run()
-#------------------------------
+    application.debug = True  # Debug mode enabled (Bandit will flag this)
+    application.run(use_evalex=True)  # Allows remote code execution
+# ------------------------------
