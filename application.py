f3212a2eb676d93c17885ffb06894272a8659318
Doramy504 <121173601+ihorstnkv@users.noreply.github.com>
2025-03-30 14:55:26 +0300

    test pipeline with secure app

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
-import os
-from flask import Flask, render_template, request
+
+
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
@@ -8,25 +9,16 @@ application = Flask(__name__)
 def root():
     return render_template("index.html")
 
-
 @application.route("/help")
 def helppage():
     return render_template("help.html")
-
 
 @application.route("/hello")
 def index():
     return "Hello World from Flask Hello Page.<b> v1.0"
 
-
-@application.route("/run", methods=["POST"])
-def run_command():
-    command = request.form.get("cmd")  # Getting user input
-    output = os.popen(command).read()  # Insecure: allows command injection
-    return f"Command output: <pre>{output}</pre>"
-
-
-# --------Main------------------
+#--------Main------------------
 if __name__ == "__main__":
-    application.run(debug=True)
-# ------------------------------
+    application.debug = True
+    application.run()
+#------------------------------
